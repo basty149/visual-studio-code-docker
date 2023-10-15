206b1a60bb9a5480f77b57abfddc0e2517a07a90
Sebastien L <basty149@gmail.com>
2023-10-15 12:22:55 +0200

    Correction du script de détection

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ code --no-sandbox
 
 # Check if VScode always running every 5 seconds
 # because of multi window's mode
-while [ $(ps -ef | grep "code --no-sandbox") ]
+while [[ $(ps -ef | grep "code --no-sandbox" | grep -v grep) ]]
 do
   sleep 5
 done
